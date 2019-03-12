f242c8037cf4a03a8cf42866eaededd564cab248
vutaka <vutaka0120@gmail.com>
2019-03-12 23:30:38 +0900

    first commit

---
+++ src/main/java/com/example/chosay/ChoSayApplication.java
@@ -0,0 +1,13 @@
+package com.example.chosay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoSayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoSayApplication.class, args);
+	}
+
+}
